c07293844606b3c49bdb49d51aa839167ffac93a
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-02 15:22:51 +0200

    Add group writing functions to io1.h
    Added new functions for writing groups to files.

--- io1.h
+++ io1.h
@@ -8,6 +8,8 @@ std::vector<Student> studentuSkaitymas(const std::string& path);
 void lentelesSpausdinimas(const std::vector<Student>& s, Mode m);
 void lentelesSpausdinimas(const std::list<Student>&    s, Mode m);
 
-void padalinimasIrIrasymas(const std::vector<Student>& s, Mode m);
-void padalinimasIrIrasymas(const std::list<Student>&    s, Mode m);
+void grupesIrasymas(const std::string& path, const std::vector<Student>& s, Mode m);
+void grupesIrasymas(const std::string& path, const std::list<Student>& s, Mode m);
+
+
 
