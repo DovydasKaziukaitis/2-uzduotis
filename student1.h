6d189220930daf7e17d894ef6e32b32b1371d510
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-14 21:55:15 +0200

    Convert Student struct to class with new members
    Refactor Student struct to class and add new enum.

--- student1.h
+++ student1.h
@@ -1,12 +1,24 @@
 #pragma once
 #include <string>
 
-struct Student {
-    std::string pav;
-    std::string var;
-    double galVid = 0;
-    double galMed = 0;
+enum class Mode { Vid = 1, Med = 2 };
+enum class Strategija {S1 = 1, S2 = 2, S3 = 3};
+
+class Student {
+private:
+    std::string pavarde_;
+    std::string vardas_;
+    double galVid_;
+    double galMed_;
+public:
+    Student()
+        : pavarde_(), vardas_(), galVid_(0.0), galMed(0.0) {}
+
+    Student(const std::string& pav, const std::string& var, double vid, double med)
+        : pavarde_(pav), vardas_(var), galVid_(vid), galMed(med) {}
 };
 
-enum class Mode { Vid = 1, Med = 2 };
+
+
+
 
