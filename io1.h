a536f20f19a372e54d002554bf211584dc40edac
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-20 21:59:43 +0200

    Reformat io1.h with consistent spacing and includes

--- io1.h
+++ io1.h
@@ -16,7 +16,7 @@ void padalinimasStrategija1(const std::vector<Student>& s, Mode m,
 void padalinimasStrategija1(const std::list<Student>& s, Mode m,
                             std::list<Student>& varg, std::list<Student>& kiet);
 
-void padalinimasStrategija2(const std::vector<Student>& s, Mode m,
+void padalinimasStrategija2(std::vector<Student>& s, Mode m,
                             std::vector<Student>& varg, std::vector<Student>& kiet);
 void padalinimasStrategija2(std::list<Student>& s, Mode m,
                             std::list<Student>& varg, std::list<Student>& kiet);
@@ -25,6 +25,7 @@ void padalinimasStrategija3(const std::vector<Student>& s, Mode m,
                             std::vector<Student>& varg, std::vector<Student>& kiet);
 void padalinimasStrategija3(std::list<Student>& s, Mode m,
                             std::list<Student>& varg, std::list<Student>& kiet);
+
 
 
 
