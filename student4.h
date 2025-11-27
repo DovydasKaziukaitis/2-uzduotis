78eeb66159f3125aff1cdbffb85cb7e179082c37
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-11-27 20:38:41 +0200

    Update and rename student1.h to student4.h

--- student4.h
+++ student4.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <string>
-#include<vector>
-#include<numeric>
-#include<algorithm>
-#include<iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
 
 enum class Mode { Vid = 1, Med = 2 };
-enum class Strategija {S1 = 1, S2 = 2, S3 = 3};
+enum class Strategija { S1 = 1, S2 = 2, S3 = 3 };
 
 class Student {
 private:
@@ -29,25 +29,28 @@ private:
         std::sort(v.begin(), v.end());
         size_t n = v.size();
         if (n % 2 == 1) return v[n / 2];
-        return (v[n/2 - 1] + v[n/2]) / 2.0;
+        return (v[n / 2 - 1] + v[n / 2]) / 2.0;
     }
 
     void perskaiciuotiIsPirminiu() {
         double vid = vidurkis(nd_);
         double med = mediana(nd_);
         galVid_ = 0.4 * egzaminas_ + 0.6 * vid;
-        galMed_ = 0.4 * egzaminas_ + 0.6 * med;}
+        galMed_ = 0.4 * egzaminas_ + 0.6 * med;
+    }
 
 public:
     Student()
-        : pavarde_(), vardas_(), galVid_(0.0), nd_(), egzaminas_(0.0), galMed_(0.0) {}
+        : pavarde_(), vardas_(), nd_(), egzaminas_(0.0), galVid_(0.0), galMed_(0.0) {}
 
     Student(const std::string& pav, const std::string& var, double vid, double med)
         : pavarde_(pav), vardas_(var), nd_(), egzaminas_(0.0), galVid_(vid), galMed_(med) {}
-    Student(const std::string& pav, const std::string& var, const std::vector<double>& nd, double egz)
+
+    Student(const std::string& pav, const std::string& var,
+            const std::vector<double>& nd, double egz)
         : pavarde_(pav), vardas_(var),
           nd_(nd), egzaminas_(egz) {
-          perskaiciuotiIsPirminiu();
+        perskaiciuotiIsPirminiu();
     }
 
     Student(const Student& kitas)
@@ -58,31 +61,29 @@ public:
           galVid_(kitas.galVid_),
           galMed_(kitas.galMed_) {}
 
-    
     Student& operator=(const Student& kitas) {
-        if(this != &kitas) {
-            pavarde_ = kitas.pavarde_;
-            vardas_ = kitas.vardas_;
-            nd_ = kitas.nd_;
+        if (this != &kitas) {
+            pavarde_   = kitas.pavarde_;
+            vardas_    = kitas.vardas_;
+            nd_        = kitas.nd_;
             egzaminas_ = kitas.egzaminas_;
-            galVid_ = kitas.galVid_;
-            galMed_ = kitas.galMed_;
+            galVid_    = kitas.galVid_;
+            galMed_    = kitas.galMed_;
         }
         return *this;
     }
-        
     ~Student() {}
 
     inline const std::string& getPavarde()  const { return pavarde_; }
     inline const std::string& getVardas()   const { return vardas_;  }
-    inline double getGalVid()   const { return galVid_;  }
-    inline double getGalMed()   const { return galMed_;  }
+    inline double getGalVid()               const { return galVid_;  }
+    inline double getGalMed()               const { return galMed_;  }
 
-    inline const std::vector<double>& getNd() const {return nd_; }
+    inline const std::vector<double>& getNd() const { return nd_; }
     inline double getEgz() const { return egzaminas_; }
 
     inline void setPavarde(const std::string& pav) { pavarde_ = pav; }
-    inline void setVardas(const std::string& var) { vardas_ = var; }
+    inline void setVardas(const std::string& var)  { vardas_ = var; }
 
     inline void setGalVid(double v) { galVid_ = v; }
     inline void setGalMed(double m) { galMed_ = m; }
@@ -95,16 +96,19 @@ public:
         egzaminas_ = egz;
         perskaiciuotiIsPirminiu();
     }
+
     friend std::istream& operator>>(std::istream& is, Student& st);
     friend std::ostream& operator<<(std::ostream& os, const Student& st);
 };
 
 inline std::istream& operator>>(std::istream& is, Student& st) {
     std::string pav, var;
-    double vid, med;
+    double vid;
 
     if (!(is >> pav >> var >> vid))
         return is;
+
+    double med;
     if (is.peek() != '\n' && is.peek() != EOF) {
         if (!(is >> med)) med = vid;
     } else {
@@ -116,28 +120,13 @@ inline std::istream& operator>>(std::istream& is, Student& st) {
     st.egzaminas_ = 0.0;
     st.galVid_    = vid;
     st.galMed_    = med;
-
     return is;
 }
-
-
 inline std::ostream& operator<<(std::ostream& os, const Student& st) {
-    os << st.pavarde_
-       << st.vardas_
-       << st.galVid_
+    os << st.pavarde_ << ' '
+       << st.vardas_  << ' '
+       << st.galVid_  << ' '
        << st.galMed_;
     return os;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
