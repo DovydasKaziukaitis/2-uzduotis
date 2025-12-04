50fdc80396d4eada594bc78f4f246d9e977d96fd
DovydasKaziukaitis <dovydaskaziukaitisd@gmail.com>
2025-12-04 22:12:33 +0200

    Update and rename io1.h to io6.h

---
+++ io6.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "student6.h"
+#include <list>
+
+std::vector<Student> skaityti_studentus(const std::string& path);
+void spausdinti_lentele(const std::vector<Student>& s, Mode m);
+void spausdinti_lentele_abus(const std::vector<Student>& s);
+void padalinti_ir_irasyti(std::vector<Student>& s, Mode m);
+void padalinti_ir_irasyti(std::list<Student>& s, Mode m);
+std::vector<Student> skaityti_rezultatus(const std::string& path);
+
+
+
+
